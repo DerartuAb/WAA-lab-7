0446337fb74b36022c4f89e712bfd7eec8126adb
DerartuAb <dabdisa@miu.edu>
2023-02-01 02:00:29 -0600

    WAA-Lab7

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Dashboard from './container/Dashboard';
+import PostDetails from './component/PostDetails';
+
+function App() {
+  return (
+    <>
+    <Dashboard/>
+    </>
+   
+  );
+}
+
+export default App;
